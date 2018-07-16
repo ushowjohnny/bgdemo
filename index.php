af657d7ca5a8f3fce8dde2db46e559ba5555a96e
ushowjohnny <johnnychui06@gmail.com>
2018-07-16 11:19:13 +0800

    change 0715 11:20

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
             <li><a href="#">Home</a></li>
             <li>Basic</li>
           </ol>
-          <h1>Welcome to an OpenShift Application!</h1>
+          <h1>Welcome to an Johnny's OpenShift Application!</h1>
           <p>The purpose of this application is to demonstrate several interesting features about OpenShift. We hope you enjoy it!</p>
         </div>
       </div>
@@ -90,7 +90,7 @@
         <div class="col-md-6 col-md-offset-3">
           <h2>Application Example</h2>
           <p>
-            <img src="bluerose.jpeg">
+            <img src="greenrose.jpeg">
           </p>
         </div>
       </div>
